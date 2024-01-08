a58ea625309ec1c826c00b005d6f3c602b9dbc62
Tristan-Laine <tristan.laine711@gmail.com>
2024-01-08 21:28:24 +0100

    Player movement OK

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,52 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerControler : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody rigidbody;
+    private Vector2 movement;
+    [SerializeField] private float speed = 5.0f;
 
-    [SerializeField]
-    private float speed = 5.0f;
-    private Vector3 movement;
-    private Vector3 moveDirection;
-    public Vector3 MoveDirection
+    void OnMove(InputValue value)
     {
-        get { return moveDirection; }
-    }
-    private float rotationSpeed = 50.0f;
-
-    public bool isAlive = true;
-
-    void Awake()
-    {
-        rigidbody = GetComponent<Rigidbody>();
+        movement = value.Get<Vector2>();
     }
 
     void Update()
     {
-        if (isAlive)
-        {
-            moveDirection = new Vector3(movement.x, 0, movement.y).normalized * speed * Time.deltaTime;
-            transform.Translate(moveDirection, Space.World);
+        // Convertit le mouvement en direction relative à la rotation du joueur
+        Vector3 move = transform.right * movement.x + transform.forward * movement.y;
 
+        // Applique le mouvement sans changer la hauteur (axe Y)
+        transform.Translate(move * speed * Time.deltaTime, Space.World);
 
-            // Player rotation
-            Vector3 mousePosition = Input.mousePosition;
-            Vector3 lookAt = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, Camera.main.transform.position.y - transform.position.y));
-
-            lookAt.y = transform.position.y;
-            Vector3 direction = (lookAt - transform.position).normalized;
-            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
-        }
-    }
-
-    void OnMove(InputValue value)
-    {
-        if (isAlive)
-        {
-            movement = value.Get<Vector3>();
-        }
     }
 }
