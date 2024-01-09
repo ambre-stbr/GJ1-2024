a97cd5189d2566d42f2c68c54f22d74780db3e49
tristanlaine <tristan.laine711@gmail.com>
2024-01-09 16:43:37 +0100

    Modification nounours et animation en cours

--- Assets/scripts/Enemy/NavigationEnemy.cs
+++ Assets/scripts/Enemy/NavigationEnemy.cs
@@ -8,39 +8,66 @@ public class NavigationEnemy : MonoBehaviour
     private GameManager gameManager;
     private NavMeshAgent agent;
     public Transform player;
-    [SerializeField] private float attackDistance = 5f;
+    public float sightRange = 10f;
+    public float attackDistance = 5f;
+    private Vector3 wanderTarget = Vector3.zero;
+    private bool isWandering = false;
 
-
-
-    // Start is called before the first frame update
     void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
         agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        agent.destination = player.position;
-
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-        if (distanceToPlayer < attackDistance)
-        {
-            Debug.Log("distanceToPlayer < attackDistance !");
-            gameManager.ShowGameOverScreen();
 
+        if (distanceToPlayer < sightRange)
+        {
+            agent.SetDestination(player.position);
+            if (distanceToPlayer < attackDistance)
+            {
+                Debug.Log("Attack Player");
+                gameManager.ShowGameOverScreen();
+            }
+        }
+        else
+        {
+            if (!isWandering)
+            {
+                StartCoroutine(Wander());
+            }
+        }
+    }
+
+    IEnumerator Wander()
+    {
+        isWandering = true;
+
+        float wanderRadius = 10f;
+        float wanderDistance = 10f;
+        float wanderJitter = 1f;
+
+        wanderTarget += new Vector3(Random.Range(-1.0f, 1.0f) * wanderJitter, 0, Random.Range(-1.0f, 1.0f) * wanderJitter);
+        wanderTarget.Normalize();
+        wanderTarget *= wanderRadius;
+
+        Vector3 targetLocal = wanderTarget + new Vector3(0, 0, wanderDistance);
+        Vector3 targetWorld = transform.TransformPoint(targetLocal);
+
+        agent.SetDestination(targetWorld);
+
+        // Attendre jusqu'à ce que l'ennemi atteigne le point cible
+        while (Vector3.Distance(transform.position, targetWorld) > agent.stoppingDistance)
+        {
+            yield return null;
         }
 
-    }
-    /*    private void OnCollisionEnter(Collision collision)
-        {
-            Debug.Log("OnCollisionEnter !");
-            if (collision.gameObject.CompareTag("Player"))
-            {
-                gameManager.ShowGameOverScreen();
+        // Attendre pendant un certain temps après avoir atteint le point
+        float waitTime = 3f; // Temps d'arrêt en secondes
+        yield return new WaitForSeconds(waitTime);
 
-                Debug.Log("ShowGameOverScreen !");
-            }
-        }*/
+        isWandering = false;
+    }
 }
