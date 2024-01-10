4b08ed816fe3e623cf743303b49409188105630c
tristanlaine <tristan.laine711@gmail.com>
2024-01-10 15:00:41 +0100

    player sprint en developement

--- Assets/scripts/Player/PlayerController.cs
+++ Assets/scripts/Player/PlayerController.cs
@@ -5,6 +5,13 @@ public class PlayerMovement : MonoBehaviour
 {
     private Vector2 movement;
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float sprintSpeed = 10.0f;
+    [SerializeField] private float sprintDuration = 5.0f;
+    [SerializeField] private float sprintCooldown = 5.0f;
+    private float sprintTimeRemaining;
+    private float sprintCooldownRemaining;
+    private bool isSprinting = false;
+
 
     void OnMove(InputValue value)
     {
@@ -14,11 +21,71 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        if (Keyboard.current.shiftKey.isPressed && sprintTimeRemaining > 0)
+        {
+            isSprinting = true;
+            sprintTimeRemaining -= Time.deltaTime;
+        }
         // Convertit le mouvement en direction relative à la rotation du joueur
         Vector3 move = transform.right * movement.x + transform.forward * movement.y;
 
         // Applique le mouvement sans changer la hauteur (axe Y)
         transform.Translate(move * speed * Time.deltaTime, Space.World);
 
+    }
+}
+
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 movement;
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float sprintSpeed = 10.0f;
+    [SerializeField] private float sprintDuration = 5.0f;
+    [SerializeField] private float sprintCooldown = 5.0f;
+    private float sprintTimeRemaining;
+    private float sprintCooldownRemaining;
+    private bool isSprinting = false;
+
+    void OnMove(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+
+    void Update()
+    {
+        HandleSprint();
+
+        float currentSpeed = isSprinting ? sprintSpeed : speed;
+        Vector3 move = transform.right * movement.x + transform.forward * movement.y;
+        transform.Translate(move * currentSpeed * Time.deltaTime, Space.World);
+    }
+
+    void HandleSprint()
+    {
+        if (sprintCooldownRemaining > 0)
+        {
+            sprintCooldownRemaining -= Time.deltaTime;
+            return;
+        }
+
+        if (Keyboard.current.shiftKey.isPressed && sprintTimeRemaining > 0)
+        {
+            isSprinting = true;
+            sprintTimeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            if (isSprinting)
+            {
+                sprintCooldownRemaining = sprintCooldown;
+                sprintTimeRemaining = sprintDuration;
+            }
+
+            isSprinting = false;
+        }
     }
 }
